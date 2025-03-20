3df1cfc0ed8c34b0df7652ba7a588c9a7693a533
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-20 14:37:06 +0100

    [UPD] Initial task 1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Creao una lista di email di invitati
+//Faccio inserire all'utente la sua email
+//creo la variabile di controllo per verificare che la sua email sia presente nella lista e la imposto su false
+//creo un ciclo di array con le email
+//verifico che la sua email inserita sia ugiuale a quella nella lista
+//se é uguale puó accedere alla festa e la variabile cambia in true
+//fuori dal controllo stampo il messaggio "Benvenuta alla festa"
